884578969ebe51222a3479b1b5d09a1c818020cc
jdlaubrie <joandlaubrie@gmail.com>
2022-12-12 21:34:03 +0100

    other update

--- python/fitting_polymer.py
+++ python/fitting_polymer.py
@@ -4,101 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-def fcirc(x, c10, k1, k2):
-    kappa = 0.
-    beta1 = 45.0*np.pi/180.0
-    beta2 = -45.0*np.pi/180.0
-    stress = 2.*c10*(x**2 - 1./x**4) + \
-             2.*k1*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))*\
-             np.exp(k2*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))**2)*\
-             (kappa*(x**2 - 1./x**4)+(1.-3.*kappa)*(x*np.cos(beta1))**2) + \
-             2.*k1*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))*\
-             np.exp(k2*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))**2)*\
-             (kappa*(x**2 - 1./x**4)+(1.-3.*kappa)*(x*np.cos(beta2))**2)
+def func_stress(x, c10):
+    stress = 2.*c10*(x**2 - 1./x)
     return stress
     
-def flong(x, c10, k1, k2):
-    kappa = 0.
-    beta1 = 45.0*np.pi/180.0
-    beta2 = -45.0*np.pi/180.0
-    stress = 2.*c10*(x**2 - 1./x**4) + \
-             2.*k1*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))*\
-             np.exp(k2*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))**2)*\
-             (kappa*(x**2 - 1./x**4)+(1.-3.*kappa)*(x*np.sin(beta1))**2) + \
-             2.*k1*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))*\
-             np.exp(k2*(kappa*(2.*x**2+1./x**4-3.)+(1.-3.*kappa)*(x**2-1.0))**2)*\
-             (kappa*(x**2 - 1./x**4)+(1.-3.*kappa)*(x*np.sin(beta2))**2)
-    return stress
-
 #--------------------------------------------------------------------------
 #          experimental data
 #--------------------------------------------------------------------------
 import pandas as pd
-file_data = open('peuu_R000V03.dat', 'r')
-data_lines = file_data.readlines()
-data_frame = pd.Series(data_lines)
-data_frame = data_frame.replace(r'\n','', regex=True)
+file_data = open('polymer_data.dat', 'r')   #open data file
+data_lines = file_data.readlines()          #read lines of the file
+data_frame = pd.Series(data_lines)          #give the lines to pandas
+data_frame = data_frame.replace(r'\n','', regex=True)   #remove end-lines
 data_frame = data_frame.replace(r'\r\n','', regex=True)
 data_frame = data_frame.replace(r'\r','', regex=True)
 #--------------------------------------------------------------------------
 for line in data_frame:
-    tmp_df = data_frame.str.strip()
-    tmp_df = tmp_df.str.split('  ',expand=True)
-    np.array(tmp_df.values, dtype=float)
+    tmp_df = data_frame.str.strip()             #remove leading and trailing char.
+    tmp_df = tmp_df.str.split('  ',expand=True) #split columns
+    np.array(tmp_df.values, dtype=float)        #create an array from the data
     exp_data = np.array(tmp_df.values, dtype=float)
 
 #--------------------------------------------------------------------------
 #          curves fitting
 #--------------------------------------------------------------------------
-ndata = 10
-# stretch lambda_1 and lambda_2
+ndata = exp_data.shape[0]
+# stretch lambda_1
 xdata = exp_data[:ndata,0]
-# bulge stress
+# stress sigma_1
 ydata = exp_data[:ndata,1]*xdata**2
 
-# 1st fit is with long
-popt1, pcov1 = curve_fit(flong, xdata, ydata,
-    p0=[563.9, 1.0, 1.0], 
-    #bounds=([0.0],[np.inf]))
-    bounds=([100.0, 0.0, 0.0],[np.inf, np.inf, np.inf]))
-print(popt1)
-# 2nd fit is with circ
-popt2, pcov2 = curve_fit(fcirc, xdata, ydata,
-    p0=popt1, 
-    #bounds=([0.0],[np.inf]))
-    bounds=([100.0, 0.0, 0.0],[np.inf, np.inf, np.inf]))
-print(popt2)
+# fitting. func, data(x,y), guess, bounds
+popt, pcov = curve_fit(func_stress, xdata, ydata,
+    p0=[500.0], bounds=([50.0],[np.inf]))
+print("C10 = {}".format(popt[0]))
 
+#compute the determination coefficient with ML library
 from sklearn.metrics import r2_score
 y_true = ydata
-# longitudinal
-y1_pred = flong(xdata, *popt2)
-deter1 = r2_score(y_true, y1_pred)
-print("Longitudinal determination: {}".format(deter1))
-# circumferential
-y2_pred = fcirc(xdata, *popt2)
-deter2 = r2_score(y_true, y2_pred)
-print("Circumferential determination: {}".format(deter2))
-
-#--------------------------------------------------------------------------
-#          fem simulation results
-#--------------------------------------------------------------------------
-import pandas as pd
-file_data = open('fem_data.dat', 'r')
-data_lines = file_data.readlines()
-data_frame = pd.Series(data_lines)
-data_frame = data_frame.replace(r'\n','', regex=True)
-data_frame = data_frame.replace(r'\r\n','', regex=True)
-data_frame = data_frame.replace(r'\r','', regex=True)
-#--------------------------------------------------------------------------
-for line in data_frame:
-    tmp_df = data_frame.str.strip()
-    tmp_df = tmp_df.str.split(' ',expand=True)
-    np.array(tmp_df.values, dtype=float)
-    fem_data = np.array(tmp_df.values, dtype=float)
-
-xfem = 1.0 + fem_data[:,1]/3.0
-yfem = 1000.0*fem_data[:,3]
+y_pred = func_stress(xdata, *popt)
+deter = r2_score(y_true, y_pred)
+print("Determination Coefficient: {}".format(deter*100.0))
 
 #--------------------------------------------------------------------------
 #          curves visualization. graphics
@@ -106,31 +52,26 @@ yfem = 1000.0*fem_data[:,3]
 xfit = np.linspace(1.0,1.07,num=15)
 fig, ax = plt.subplots(constrained_layout=True)
 
-ax.plot(xfit, flong(xfit, *popt2), 'b-', label='Longitudinal fit')
-ax.plot(xfit, fcirc(xfit, *popt2), 'r-', label='Circumferential fit')
-ax.plot(xfem, yfem, 'g-', label='Abaqus simulation')
-ax.plot(exp_data[:,0], exp_data[:,1]*(exp_data[:,0])**2, 'o')
+# plot curve and data
+ax.plot(xfit, func_stress(xfit, *popt), 'b-', label='Fit')
 ax.plot(xdata, ydata, 'o', label='Experimental data')
 
+# make the graphic nicer
 ax.set_xlabel('stretch [-]',fontsize=14)
 ax.set_ylabel('stress [kPa]',fontsize=14)
 ax.set_ylim(bottom=0,top=500)
 ax.set_xlim(left=1.0,right=1.1)
 for label in (ax.get_xticklabels()+ax.get_yticklabels()):
     label.set_fontsize(14)
-
+#open the box and add legend
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.legend(loc='upper left',fontsize=14)
 
 plt.show
-
+#file output
 FIGURENAME = 'fitting_py.eps'
 plt.savefig(FIGURENAME)
-
+#close graphical tools
 plt.close('all')
-
-#[ 100.         1017.0845917     9.36312794]
-#Longitudinal determination: 0.9724576302156388
-#Circumferential determination: 0.9724576302156389
 
--- python/newton_raphson.py
+++ python/newton_raphson.py
@@ -1,81 +1,15 @@
-# Mathematics libraries
+# Mathematical libraries
 import numpy as np
 
-def residual_func(time,Delta_t,Y,density,den_):
-    # Elastin degradation [days]
-    L_dam = 0.010
-    t_dam = 40.0
-    D_max = 0.5
-    T_ela = 101.0*365.25
-    density0 = 241.5
-    # Residual function
-    exponential = np.exp(-0.5*(Y/L_dam)**2 - time/t_dam)
-    residual = -density + den_ + Delta_t*(-density/T_ela-(D_max/t_dam)*np.multiply(exponential,density0))
-    return residual
-
-def df_ridders(time,Delta_t,Y,density,den_):
-    """
-    Returns the derivative of a function func at a point x by Ridders method of polynomial
-    extrapolation. The value h is input as an estimated initial stepsize; it need not be small,
-    but rather should be an increment in x over which func changes substantially. An estimate
-    of the error in the derivative is returned as err .
-    Parameters: Stepsize is decreased by CON at each iteration. Max size of tableau is set by
-    NTAB. Return when error is SAFE worse than the best so far.
-    """
-    h=1.0e-3
-    BIG=1.0e30
-    NTAB=10
-    CON=1.4
-    CON2=CON*CON
-    SAFE=2.0
-    if h==0.0:
-        print('h must be nonzero in df_ridders')
-        return
-    a = np.zeros((NTAB,NTAB),dtype=np.float64)
-    hh=h
-    x_pos = density + hh
-    x_neg = density - hh
-    func_pos = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=x_pos,den_=den_)
-    func_neg = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=x_neg,den_=den_)
-    a[0,0] = (func_pos-func_neg)/(2.0*hh)
-    err=BIG
-    # Successive columns in the Neville tableau will go to smaller stepsizes and higher orders of extrapolation.
-    for i in range(1,NTAB):
-        hh=hh/CON
-        # Try new, smaller stepsize.
-        x_pos = density + hh
-        x_neg = density - hh
-        func_pos = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=x_pos,den_=den_)
-        func_neg = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=x_neg,den_=den_)
-        a[0,i] = (func_pos-func_neg)/(2.0*hh)
-        fac=CON2
-        # Compute extrapolations of various orders, requiring no new function evaluations.
-        for j in range(1,i+1):
-            a[j,i] = (a[j-1,i]*fac-a[j-1,i-1])/(fac-1.0)
-            fac=CON2*fac
-            errt=max(abs(a[j,i]-a[j-1,i]),abs(a[j,i]-a[j-1,i-1]))
-            #The error strategy is to compare each new extrapolation to one order lower, both at
-            #the present stepsize and the previous one.
-            #If error is decreased, save the improved answer.
-            if (errt<=err):
-                err=errt
-                dfridr=a[j,i]
-        #If higher order is worse by a significant factor SAFE, then quit early.
-        if (abs(a[i,i]-a[i-1,i-1])>=SAFE*err):
-            #print('Early quit in df_ridders function')
-            return dfridr,err
-    return dfridr,err
-
-def func_eval(time,Delta_t,Y,density,den_):
+def func_eval(time):
     """
     Function with the aim to get the values of the function and derivative at x.
     """
-    h=1.0e-3
-    func = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=density,den_=den_)
-    df,err = df_ridders(time=time,Delta_t=Delta_t,Y=Y,density=density,den_=den_)
-    return func,df,err
+    func = 120.0*np.sin(np.pi*time/0.3) - 7.6
+    df = 120.0*(np.pi/0.3)*np.cos(np.pi*time/0.3)
+    return func,df
 
-def newton_raphson(time,Delta_t,Y,density):
+def newton_raphson(t1,t2,tol):
     """
     Using the Newton-Raphson method, find the root of a function known to lie close to x. 
     The root rtnewt will be refined until its accuracy is known within +/- xacc. funcd
@@ -84,44 +18,56 @@ def newton_raphson(time,Delta_t,Y,density):
     """
     # Set to maximum number of iterations.
     JMAX=20
-    res = residual_func(time=time,Delta_t=Delta_t,Y=Y,density=density,den_=density)
     # Initial guess.
-    rtnewt=density
+    rtnewt=0.5*(t1+t2)
+    #res,_ = func_eval(time=time)
     for j in range(JMAX):
-        func,df,err = func_eval(time=time,Delta_t=Delta_t,Y=Y,density=rtnewt,den_=density)
-        #print(func)
-        #print(df)
+        func,df = func_eval(rtnewt)
         dx = func/df
-        #print(dx)
         rtnewt -= dx
-        #print(rtnewt)
+        # Error
+        if (t1-rtnewt)*(rtnewt-t2)<0.0:
+            print("rtnewt jumped out of brackets")
+            return rtnewt
         # Convergence.
-        if np.absolute(func/res)<1.e-5:
-            #print('Iterations: '+str(j))
+        if np.absolute(dx)<tol: #np.absolute(func/res)<tolerance:
             return rtnewt
     print('newton_raphson exceeded maximum iterations')
     return rtnewt
 
 # Test of the Solution method with derivatives from Ridders method
-T_e = 101*365.25
-Dm = 0.5
-td = 40.0
-Y=0.001
-Ld=0.010
-den_=241.5
-dens=241.5
-# time
-Delta_t=10.0
-time = 0.0
-while time<10:
-    time += Delta_t
-    backeuler = newton_raphson(time=time,Delta_t=Delta_t,Y=Y,density=den_)
-    func,df,err = func_eval(time=time,Delta_t=Delta_t,Y=Y,density=backeuler,den_=den_)
-    density=241.5*np.exp(-time/T_e)+241.5*(Dm/td)*(T_e*td/(td-T_e))*np.exp(-0.5*(Y/Ld)**2)*\
-        (np.exp(-time/T_e)-np.exp(-time/td))
-    exponential = np.exp(-0.5*(Y/0.010)**2 - (time-Delta_t)/40.0)
-    forweuler = dens + Delta_t*(-dens/(101.*365.25)-(0.5/40.)*np.multiply(exponential,241.5))
-    dens = forweuler
-    den_=backeuler
-    print(time,forweuler,backeuler,density)
+t1 = 0.2
+t2 = 0.35
+tol = 1.0e-5
+solution = newton_raphson(t1,t2,tol)
+print(solution)
+
+#--------------------------------------------------------------------------
+#          curves visualization. graphics
+#--------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+time = np.linspace(0.0,0.3,num=15)
+cte = 7.6*np.ones(15)
+fig, ax = plt.subplots(constrained_layout=True)
+
+# plot curve and data
+ax.plot(time, 120.0*np.sin(np.pi*time/0.3), 'b-')
+ax.plot(time, cte, 'r-')
+
+# make the graphic nicer
+ax.set_xlabel('time [s]',fontsize=14)
+ax.set_ylabel('pressure [mmHg]',fontsize=14)
+#ax.set_ylim(bottom=0,top=500)
+#ax.set_xlim(left=1.0,right=1.1)
+for label in (ax.get_xticklabels()+ax.get_yticklabels()):
+    label.set_fontsize(14)
+ax.grid(True)
+#ax.legend(loc='upper left',fontsize=14)
+
+plt.show()
+#file output
+#FIGURENAME = 'fitting_py.eps'
+#plt.savefig(FIGURENAME)
+#close graphical tools
+plt.close('all')
 
